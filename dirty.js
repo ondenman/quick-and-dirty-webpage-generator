312bfd3003ccdf0693ac0489942f28ae59044861
Oliver Denman <oliver@therewillbeanimals.com>
2016-06-05 00:46:35 +0100

    Checks whether input and output filenames differ

--- dirty.js
+++ dirty.js
@@ -1,34 +1,42 @@
 'use strict'
 
 const fs = require ('fs')
+const marked = require ('marked')
 
-function fileIsValid(file) {
-    let type = file.split('.')[1]
-    return type === 'md' || type === 'mmd'
-}
+const CONTENT_DIR = './site/content/'
+const DIR = './site/'
 
 function listMarkdownFiles(dir) {
     let md_files = []
-
     fs.readdirSync(dir).forEach( (file) => {
-        if (fileIsValid(file))
-            md_files.push(file)
+        md_files.push(file)
     })
-
     return md_files
 }
 
 function fileContents(file) {
-    return fs.readFileSync(file, 'utf-8')
+    return fs.readFileSync(file, 'utf-8') || false
 }
 
-function markdown(file) {
-    md = fs.readFileSync(file, 'utf-8')
-}
-
-function popolatePage(page, sections) {
+function populatePage(page, sections) {
+    console.log('message');
     sections.forEach( (s) => {
-        let tag = s.split('.')[1]
-        page.replace('{{{ '+tag+' }}}', markdown(s))
+        let md = marked(fileContents(CONTENT_DIR+s))
+        page = page.split('{{{'+s+'}}}').join(md)
     })
+    return page
 }
+
+function run(dirtyFile) {
+    const md = listMarkdownFiles(CONTENT_DIR)
+    const dirty = fileContents(DIR+dirtyFile)
+    const clean = dirty ? populatePage(dirty, md) : 'Error processing page :('
+    const cleanName = dirtyFile.split('.')[0]+'.html'
+    if (dirtyFile === cleanName)
+        console.log('Input name: '+dirtyFile+' is the same as output name: '
+                    +cleanName+'\nChange input file\'s extension. Eg. \'.dirty\'')
+    fs.writeFileSync(DIR+cleanName, clean,'utf-8')
+    console.log('All done :)')
+}
+
+run(process.argv[2])
